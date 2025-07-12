dce8cefc164a10fcad49a2353512814e2320f504
HarrySharma2008 <mine99211498@outlook.com>
2025-07-12 11:15:07 +0530

    11th commit

--- app/api/complete-profile/route.ts
+++ app/api/complete-profile/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/dbConnect';
 import User from '@/models/User';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../auth/[...nextauth]/route';
+import { authOptions } from '@/lib/auth';
 
 export async function POST(req: Request) {
   await dbConnect();
